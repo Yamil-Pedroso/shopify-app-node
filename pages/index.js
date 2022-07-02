34942593b5aaca29958dde5cd15b0ef0c1f21f49
Yami Pedroso-AI <55386637+Yamil-Pedroso@users.noreply.github.com>
2022-07-02 12:01:46 +0200

    New branch added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Heading, Page } from "@shopify/polaris";
 const Index = () => (
   <Page>
     <Heading>Testing-Plugin-Dashboard-v1</Heading>
+    <h1>Probamos hacer un pull request</h1>
   </Page>
 );
 
