04ae655cab6c2268633bdc12669d292ad8f3aed9
Yami Pedroso-AI <55386637+Yamil-Pedroso@users.noreply.github.com>
2022-07-04 13:09:27 +0200

    Added h2

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Heading, Page } from "@shopify/polaris";
 const Index = () => (
   <Page>
     <Heading>Testing-Plugin-Dashboard-v1</Heading>
+    <h2>We changed for test</h2>
   </Page>
 );
 
